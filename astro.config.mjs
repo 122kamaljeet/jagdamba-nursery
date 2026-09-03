f5c54c5225c87acd2ccdbc2b59f2edd7e7825a53
122kamaljeet <122kamaljeet@gmail.com>
2026-09-03 18:12:32 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 // Replace with your real domain once purchased/connected.
 export default defineConfig({
   site: " https://122kamaljeet.github.io/jagdamba-nursery/",
-  base: "/jagdamba-nursery/",
+  base: "/jagdamba-nursery",
   devToolbar: {
     enabled: false,
   },
